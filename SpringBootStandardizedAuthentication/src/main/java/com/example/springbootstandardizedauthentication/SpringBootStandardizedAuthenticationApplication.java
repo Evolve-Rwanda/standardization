7ccd3e7ff728bd049c7dd797e6dd7eaad4ef6241
Maurice-Mugisha <mugmau25@gmail.com>
2025-04-14 10:19:11 +0200

    starting to test UI authentication

---
+++ SpringBootStandardizedAuthentication/src/main/java/com/example/springbootstandardizedauthentication/SpringBootStandardizedAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootstandardizedauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStandardizedAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStandardizedAuthenticationApplication.class, args);
+	}
+
+}
