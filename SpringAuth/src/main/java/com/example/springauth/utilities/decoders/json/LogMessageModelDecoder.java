eda198781d58753452891dcb2ecab6e5e3564bfe
Maurice-Mugisha <mugmau25@gmail.com>
2025-06-02 16:11:25 +0200

    Working on the logger - all logger levels and targets for the application

---
+++ SpringAuth/src/main/java/com/example/springauth/utilities/decoders/json/LogMessageModelDecoder.java
@@ -0,0 +1,10 @@
+package com.example.springauth.utilities.decoders.json;
+
+public class MessageModelDecoder {
+
+    private String jsonEncodedMessageString;
+
+    public MessageModelDecoder(String jsonEncodedMessageString) {
+        this.jsonEncodedMessageString = jsonEncodedMessageString;
+    }
+}
